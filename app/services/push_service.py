3c81d8ff88b3712dd9c9b1379ac65f5e5745d2c0
Hasanabbas <hasanabbas@Hasanabbass-MacBook-Air.local>
2026-09-15 00:43:32 +0530

    add new functionality with login

--- app/services/push_service.py
+++ app/services/push_service.py
@@ -9,9 +9,15 @@ import os
 import json
 import base64
 import time
-from py_vapid import Vapid
-from cryptography.hazmat.primitives import serialization
-from pywebpush import webpush, WebPushException
+try:
+    from py_vapid import Vapid
+    from cryptography.hazmat.primitives import serialization
+    from pywebpush import webpush, WebPushException
+except ImportError:
+    Vapid = None
+    serialization = None
+    webpush = None
+    WebPushException = Exception
 from app.utils.logger import get_logger
 from app.extensions import connect_to_mongodb
 from app.services.stock_prediction_service import StockPredictionService
